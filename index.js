d6a4edb6078f6d9e95460f8c009cf18b0ea77126
Colin Clark <colinbdclark@gmail.com>
2015-10-17 15:27:41 -0400

    Adds Node.js bootstrap file.

---
+++ index.js
@@ -0,0 +1,19 @@
+/*
+Copyright 2015 Colin Clark
+
+Licensed under the 3-Clause "New" BSD license.
+You may not use this file except in compliance with one these
+Licenses.
+
+You may obtain a copy of the 3-Clause "New" BSD License at
+https://github.com/colinbdclark/infusion-electron/raw/master/LICENSE.txt
+*/
+
+"use strict";
+
+var fluid = require("infusion");
+require("src/app.js");
+require("src/core.js");
+require("src/window.js");
+
+module.exports = fluid.registerNamespace("electron");
